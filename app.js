82462abb8ed3b4421cda43ecedcc4796bfc1d18e
Bjjones8512 <bjjones8512@yahoo.com>
2024-09-24 14:26:55 -0500

    Routes app.js

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 const app = express();
 const port = 3000;
+
+// Routes
+const userRouter = require('./routes/users');
+const showRouter = require('./routes/shows');
+app.use('/users', userRouter);
+app.use('/shows', showRouter);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
