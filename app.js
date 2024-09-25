4e2eb5ce05c61c7b2a3ae5ffc824359ca0bae07e
Bjjones8512 <bjjones8512@yahoo.com>
2024-09-25 11:30:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-const userRouter = require('./routes/usersRoutes');
-const showRouter = require('./routes/showsRoutes');
+const userRouter = require('./routes/userRoutes'); // Ensure correct path to routes
+const showRouter = require('./routes/showRoutes');
 app.use('/users', userRouter);
 app.use('/shows', showRouter);
 
